376e9133b5967c5e160ea828fd16c38be4940aeb
nikita <kubrik2@mail.ru>
2024-07-30 15:19:34 +0500

    проверка hits

--- src/components/RecipesItemList/RecipesItemList.jsx
+++ src/components/RecipesItemList/RecipesItemList.jsx
@@ -1,9 +1,13 @@
 import {RecipesItem} from '../RecipeItem/RecipesItem.jsx';
+
 export const RecipesItemList = ({data}) => {
+
+    if (data.hits.length === 0) {
+        return <h1>Ничего не найдено</h1>
+    }
     return (
         <>
             {data.hits.map(item => <RecipesItem item={item}/>)}
-            {data.hits.toString() === "" ? <h1>Ничего не найдено</h1> : null}
         </>
 
     )
