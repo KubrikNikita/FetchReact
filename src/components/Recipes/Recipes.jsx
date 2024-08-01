c5654ea0ec90acc490291b44c7700a2d1b08bfd7
nikita <kubrik2@mail.ru>
2024-08-01 12:43:38 +0500

    calories

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -4,31 +4,40 @@ import {useDebounce} from "../Hooks/useDebounce.jsx";
 import {Dots} from "../Dots/";
 
 export const Recipes = () => {
-    const [inputValue, setInputValue] = useState('');
+    const [recipeInputValue, setRecipeInputValue] = useState('');
+    const [kcalInputValue, setKcalInputValue] = useState('');
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
-    const debounceInputValue = useDebounce(inputValue);
+    const link = 'https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&';
+    const nameLink = 'q=';
+    const calories = '&calories='
+    const debounceRecipeInputValue = useDebounce(recipeInputValue);
     useEffect(() => {
         const getRecipes = async () => {
             setIsLoading(true);
-            const link = 'https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&';
-            const nameLink = 'q=';
-            const response = await fetch(`${link}${nameLink}${inputValue}`);
+            const response = await fetch(`${link}${nameLink}${debounceRecipeInputValue}${kcalInputValue ? calories + kcalInputValue : ""}`);
             const json = await response.json();
             setData(json);
             setIsLoading(false);
         };
-        if (debounceInputValue) {
+        if (debounceRecipeInputValue) {
             getRecipes();
         } else {
             setData(null);
         }
-    }, [debounceInputValue]);
-
+    }, [debounceRecipeInputValue]);
+    const handleChange = async () => {
+        const response = await fetch(`${link}${nameLink}${debounceRecipeInputValue}${kcalInputValue ? calories + kcalInputValue : ""}`)
+        const json = await response.json();
+        setData(json);
+    }
     return (
         <>
-            <input className="input" type="text" value={inputValue}
-                   onChange={(item) => setInputValue(item.target.value)}/>
+            <input className="input" type="text" value={recipeInputValue}
+                   onChange={(item) => setRecipeInputValue(item.target.value)}/>
+            <input className="input" type="number" placeholder={"Calories"} value={kcalInputValue}
+                   onChange={(item) => setKcalInputValue(item.target.value)}/>
+            <button onClick={handleChange}>Учесть калории</button>
             {data !== null ? <RecipesItemList data={data}></RecipesItemList> : null}
             {isLoading ? <Dots/> : null}
 
